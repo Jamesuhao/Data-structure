093de56d3a5dff54a3e35b6d92ffb54bfe6affcd
Jamesuhao <1766727840@qq.com>
2019-10-18 18:03:58 +0800

    自我实现初级数据结构

--- Project1/Project1/ListNode.h
+++ Project1/Project1/ListNode.h
@@ -1,24 +1,24 @@
-//#pragma once
-//#include<stdio.h>
-//typedef int SLDataType;
-//typedef struct SListNode
-//{
-//	SLDataType data;
-//	struct SListNode* next;
-//}SListNode;
-//typedef struct SList
-//{
-//	SListNode* head;
-//}SList;
-//void SListInit(SList* plist);//初始化无头单向非循环链表
-//void SListDestory(SList* plist);//摧毁无头单向非循环链表
-//SListNode* BuySListNode(SLDataType x);//定义结点
-//void SListPushBack(SList* plist, SLDataType x);//尾插
-//void SListPopBack(SList* plist);//尾删
-//void SListPushFront(SList* plist, SLDataType x);//头插
-//void SListPopFront(SList* plist);//头删
-//SListNode* SListFind(SList* plist, SLDataType x);//查找
-//void SListInsertAfter(SListNode* pos, SLDataType x);//在pos位的后面进行插入
-//void SListEraseAfter(SListNode* pos);//在pos位的后面删除
-//void SListRemove(SList* plist, SLDataType x);//删除某个元素
-//void SListPrint(SList* plist);//打印链表
+#pragma once
+#include<stdio.h>
+typedef int SLDataType;
+typedef struct SListNode
+{
+	SLDataType data;
+	struct SListNode* next;
+}SListNode;
+typedef struct SList
+{
+	SListNode* head;
+}SList;
+void SListInit(SList* plist);//初始化无头单向非循环链表
+void SListDestory(SList* plist);//摧毁无头单向非循环链表
+SListNode* BuySListNode(SLDataType x);//定义结点
+void SListPushBack(SList* plist, SLDataType x);//尾插
+void SListPopBack(SList* plist);//尾删
+void SListPushFront(SList* plist, SLDataType x);//头插
+void SListPopFront(SList* plist);//头删
+SListNode* SListFind(SList* plist, SLDataType x);//查找
+void SListInsertAfter(SListNode* pos, SLDataType x);//在pos位的后面进行插入
+void SListEraseAfter(SListNode* pos);//在pos位的后面删除
+void SListRemove(SList* plist, SLDataType x);//删除某个元素
+void SListPrint(SList* plist);//打印链表
--- Project1/Project1/Stack.h
+++ Project1/Project1/Stack.h
@@ -1,19 +1,19 @@
-//#include<stdio.h>
-//#include<assert.h>
-//#include<malloc.h>
-//typedef int DataType;
-//typedef struct Stack
-//{
-//	DataType* _a;
-//	size_t _top;//Õ»¶¥
-//	size_t _capacity;//ÈÝÁ¿
-//}Stack;
-//void StackInit(Stack* st);
-//void StackDestory(Stack* st);
-//void StackPush(Stack* st, DataType x);
-//void StackPop(Stack* st);
-//DataType StackTop(Stack* st);
-//size_t StackSize(Stack* st);
-//int StackEmpty(Stack* st);
-//void StackPrint(Stack* st);
-//void TestStack();
+#include<stdio.h>
+#include<assert.h>
+#include<malloc.h>
+typedef int DataType;
+typedef struct Stack
+{
+	DataType* _a;
+	size_t _top;//Õ»¶¥
+	size_t _capacity;//ÈÝÁ¿
+}Stack;
+void StackInit(Stack* st);
+void StackDestory(Stack* st);
+void StackPush(Stack* st, DataType x);
+void StackPop(Stack* st);
+DataType StackTop(Stack* st);
+size_t StackSize(Stack* st);
+int StackEmpty(Stack* st);
+void StackPrint(Stack* st);
+void TestStack();
