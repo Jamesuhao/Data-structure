45bf8370ab6dcb51c8aab6a04735b6343361069f
Jamesuhao <1766727840@qq.com>
2019-10-21 19:10:17 +0800

    二叉树的实现

--- Btree/Btree/BinarryTree.c
+++ Btree/Btree/BinarryTree.c
@@ -1,4 +1,4 @@
-//#include"BinarryTree.h"
+#include"BinarryTree.h"
 #include"Stack.h"
 #include"QueueNode.h"
 #include<assert.h>
@@ -10,6 +10,8 @@ BTNode* BinaryTreeCreate(BTDataType* a, int* pi)
 	if (a[*pi] != '#')
 	{
 		BTNode* root = (BTNode*)malloc(sizeof(BTNode));
+		if (root == NULL)
+			return NULL;
 		root->_data = a[*pi];
 		(*pi)++;
 		root->_left = BinaryTreeCreate(a, pi);
@@ -23,24 +25,6 @@ BTNode* BinaryTreeCreate(BTDataType* a, int* pi)
 		return NULL;
 	}
 }
- //通过中序遍历的数组"#D#B#E#H#A#F#C#G#"构建二叉树 
-//BTNode* BinaryTreeCreate(BTDataType* a, int* pi)
-//{
-//	if (a[*pi] != '#')
-//	{
-//		BTNode* root = (BTNode*)malloc(sizeof(BTNode));
-//		root->_left = BinaryTreeCreate(a, pi);
-//		root->_data = a[*pi];
-//		++(*pi);
-//		root->_right = BinaryTreeCreate(a, pi);
-//		//++(*pi);
-//		return root;
-//	}
-//	else
-//	{
-//		return NULL;
-//	}
-//}
 void BinaryTreeDestory(BTNode** root)
 {
 	BTNode* curRoot = *root;
@@ -163,163 +147,165 @@ void BinaryTreePostOrder(BTNode* root)
 	//}
 }
 //层序遍历
-//void BinaryTreeLevelOrder(BTNode* root)
+void BinaryTreeLevelOrder(BTNode* root)
+{
+	Queue q;
+	QueueInit(&q);
+	//根元素入队
+	if (root)
+		QueuePush(&q,root);
+	while (QueueEmpty(&q) == 0)
+	{
+		BTNode* front = QueueFront(&q);
+		QueuePop(&q);
+		//遍历
+		printf("%c ", front->_data);
+		//孩子入队
+		if (front->_left)
+			QueuePush(&q, front->_left);
+		if (front->_right)
+			QueuePush(&q, front->_right);
+	}
+	printf("\n");
+}
+//// 判断二叉树是否是完全二叉树 
+int BinaryTreeComplete(BTNode* root)
+{
+	Queue q;
+	QueueInit(&q);
+	if (root!=NULL)
+		QueuePush(&q, root);
+	while (QueueEmpty(&q) == 0)
+	{
+		BTNode* front = QueueFront(&q);
+		QueuePop(&q);
+		//判断是否为空
+		if (front)
+		{
+			QueuePush(&q, front->_left);
+			QueuePush(&q, front->_right);
+		}
+		else
+			break;
+	}
+	while (QueueEmpty(&q) == 0)
+	{
+		BTNode* front = QueueFront(&q);
+		//判断是否为空，非空，说明不连续
+		if (front)
+		{ 
+			QueueDestory(&q);
+			return -1;
+		}
+		QueuePop(&q);
+	}
+	return 1;
+}
+//非递归前序遍历
+//void BinaryTreePrevOrderNonR(BTNode* root)
 //{
-//	Queue q;
-//	QueueInit(&q);
-//	//根元素入队
-//	if (root)
-//		QueuePush(&q,root);
-//	while (QueueEmpty(&q) == 0)
+//	BTNode* top;
+//	BTNode* cur = root;
+//	Stack st;
+//	StackInit(&st);
+//	while (cur||StackEmpty(&st)==0)
 //	{
-//		BTNode* front = QueueFront(&q);
-//		QueuePop(&q);
-//		//遍历
-//		printf("%c ", front->_data);
-//		//孩子入队
-//		if (front->_left)
-//			QueuePush(&q, front->_left);
-//		if (front->_right)
-//			QueuePush(&q, front->_right);
+//		while (cur)
+//		{
+//			printf("%c ", cur->_data);
+//			StackPush(&st, cur);
+//			cur = cur->_left;
+//		}
+//		//最后一棵子树的右子树
+//		top = StackTop(&st);
+//		StackPop(&st);
+//		cur = top->_right;
 //	}
 //	printf("\n");
 //}
-//// 判断二叉树是否是完全二叉树 
-//int BinaryTreeComplete(BTNode* root)
+////非递归中序遍历
+//void BinaryTreeInOrderNonR(BTNode* root)
 //{
-//	Queue q;
-//	QueueInit(&q);
-//	if (root)
-//		QueuePush(&q, root);
-//	while (QueueEmpty(&q) == 0)
+//	BTNode* top;
+//	BTNode* cur = root;
+//	Stack st;
+//	StackInit(&st);
+//	while (cur || StackEmpty(&st) == 0)
 //	{
-//		BTNode* front = QueueFront(&q);
-//		QueuePop(&q);
-//		//判断是否为空
-//		if (front)
+//		while (cur)
 //		{
-//			QueuePush(&q, front->_left);
-//			QueuePush(&q, front->_right);
+//			StackPush(&st, cur);
+//			cur = cur->_left;
 //		}
-//		else
-//			break;
+//		top = StackTop(&st);
+//		printf("%c ", top->_data);
+//		StackPop(&st);
+//		cur = top->_right;
 //	}
-//	while (QueueEmpty(&q) == 0)
-//	{
-//		BTNode* front = QueueFront(&q);
-//		//判断是否为空，非空，说明不连续
-//		if (front)
-//		{
-//			QueueDestory(&q);
-//			return -1;
-//		}
-//		QueuePop(&q);
-//	}
-//	return 1;
+//	printf("\n");
 //}
-//非递归前序遍历
-void BinaryTreePrevOrderNonR(BTNode* root)
-{
-	BTNode* top;
-	BTNode* cur = root;
-	Stack st;
-	StackInit(&st);
-	while (cur||StackEmpty(&st)==0)
-	{
-		while (cur)
-		{
-			printf("%c ", cur->_data);
-			StackPush(&st, cur);
-			cur = cur->_left;
-		}
-		//最后一棵子树的右子树
-		top = StackTop(&st);
-		StackPop(&st);
-		cur = top->_right;
-	}
-	printf("\n");
-}
-void BinaryTreeInOrderNonR(BTNode* root)
-{
-	BTNode* top;
-	BTNode* cur = root;
-	Stack st;
-	StackInit(&st);
-	while (cur || StackEmpty(&st) == 0)
-	{
-		while (cur)
-		{
-			StackPush(&st, cur);
-			cur = cur->_left;
-		}
-		top = StackTop(&st);
-		printf("%c ", top->_data);
-		StackPop(&st);
-		cur = top->_right;
-	}
-	printf("\n");
-}
-//非递归后序遍历
-void BinaryTreePostOrderNonR(BTNode* root)
-{
-	BTNode* prev=NULL;
-	BTNode* top;
-	BTNode* cur = root;
-	Stack st;
-	StackInit(&st);
-	while (cur || StackEmpty(&st) == 0)
-	{
-		while (cur)
-		{
-			StackPush(&st, cur);
-			cur = cur->_left;
-		}
-		//最后一棵子树的右子树
-		top = StackTop(&st);
-		//判断是否存在右子树,不存在，那就访问根，出栈，如果存在，要先访问右子树,如果右子树已经访问完，可以访问根
-		if(top->_right == NULL || top->_right == prev)
-		{
-			printf("%c ", top->_data);
-			StackPop(&st);
-			prev = top;
-		}
-		//存在右子树，且没有访问，先访问右子数
-		else
-		{
-			cur = top->_right;
-		}
-	}
-	printf("\n");
-}
+////非递归后序遍历
+//void BinaryTreePostOrderNonR(BTNode* root)
+//{
+//	BTNode* prev=NULL;
+//	BTNode* top;
+//	BTNode* cur = root;
+//	Stack st;
+//	StackInit(&st);
+//	while (cur || StackEmpty(&st) == 0)
+//	{
+//		while (cur)
+//		{
+//			StackPush(&st, cur);
+//			cur = cur->_left;
+//		}
+//		//最后一棵子树的右子树
+//		top = StackTop(&st);
+//		//判断是否存在右子树,不存在，那就访问根，出栈，如果存在，要先访问右子树,如果右子树已经访问完，可以访问根
+//		if(top->_right == NULL || top->_right == prev)
+//		{
+//			printf("%c ", top->_data);
+//			StackPop(&st);
+//			prev = top;
+//		}
+//		//存在右子树，且没有访问，先访问右子数
+//		else
+//		{
+//			cur = top->_right;
+//		}
+//	}
+//	printf("\n");
+//}
 
-void TestBinarryTree()
-{   //ABD##E#H##CF##G##
-	//#D#B#E#H#A#F#C#G#
-	int i = 0;
-	char a[] = { 'A','B','D','#','#','E','#','H','#','#','C','F','#','#','G','#','#' };
-	BTNode* root = BinaryTreeCreate(a, &i);
-	//递归前序遍历
-	BinaryTreePrevOrder(root);
-	printf("\n");
-	//非递归前序遍历
-	BinaryTreePrevOrderNonR(root);
-	//递归中序遍历
-	BinaryTreeInOrder(root);
-	printf("\n");
-	//非递归中序遍历
-	BinaryTreeInOrderNonR(root);
-	//递归后序遍历
-	BinaryTreePostOrder(root);
-	printf("\n");
-	//非递归后序遍历
-	BinaryTreePostOrderNonR(root);
-	//i = BinaryTreeLeafSize(root);
-	//printf("%d\n",i);
-	//i = BinaryTreeSize(root);
-	//printf("%d\n", i);
-}
 //void TestBinarryTree()
 //{   //ABD##E#H##CF##G##
+//	//#D#B#E#H#A#F#C#G#
+//	int i = 0;
+//	char a[] = { 'A','B','D','#','#','E','#','H','#','#','C','F','#','#','G','#','#' };
+//	BTNode* root = BinaryTreeCreate(a, &i);
+//	//递归前序遍历
+//	BinaryTreePrevOrder(root);
+//	printf("\n");
+//	//非递归前序遍历
+//	BinaryTreePrevOrderNonR(root);
+//	//递归中序遍历
+//	BinaryTreeInOrder(root);
+//	printf("\n");
+//	//非递归中序遍历
+//	BinaryTreeInOrderNonR(root);
+//	//递归后序遍历
+//	BinaryTreePostOrder(root);
+//	printf("\n");
+//	//非递归后序遍历
+//	BinaryTreePostOrderNonR(root);
+//	//i = BinaryTreeLeafSize(root);
+//	//printf("%d\n",i);
+//	//i = BinaryTreeSize(root);
+//	//printf("%d\n", i);
+//}
+void TestBinarryTree()
+{   
+	//ABD##E#H##CF##G##
 //	//#D#B#E#H#A#F#C#G#
 //	//int i = 0;
 //	//char a[] = { 'A','B','D','#','#','E','#','H','#','#','C','F','#','#','G','#','#' };
@@ -327,22 +313,22 @@ void TestBinarryTree()
 //	//BinaryTreePrevOrder(root);
 //	//printf("\n");
 //	//BinaryTreeLevelOrder(root);
-//	int i = 0;
-//	int ret1 = 0;
-//	int ret2 = 0;
-//	//非平衡二叉树
-//	char a1[] = { 'A','B','D','G','#','#','H','#','#','#','C','E','#','I','#','#','F','#','#' };
-//	//平衡二叉树
-//	char a2[] = { 'A','B','D','G','#','#','H','#','#','Z','#','#','C','E','#','#','F','#','#' };
-//	BTNode* root1 = BinaryTreeCreate(a1, &i);
-//	i = 0;
-//	BTNode* root2 = BinaryTreeCreate(a2, &i);
-//	BinaryTreeLevelOrder(root1);
-//	BinaryTreeLevelOrder(root2);
-//	ret1 = BinaryTreeComplete(root1);
-//	ret2 = BinaryTreeComplete(root2);
-//	printf("ret1=%d ret2=%d\n", ret1, ret2);
-//}
+	int i = 0;
+	int ret1 = 0;
+	int ret2 = 0;
+	//非平衡二叉树
+	char a1[] = { 'A','B','D','G','#','#','H','#','#','#','C','E','#','I','#','#','F','#','#' };
+	//平衡二叉树
+	char a2[] = { 'A','B','D','G','#','#','H','#','#','Z','#','#','C','E','#','#','F','#','#' };
+	BTNode* root1 =BinaryTreeCreate(a1, &i);
+	i = 0;
+	BTNode* root2 = BinaryTreeCreate(a2, &i);
+	BinaryTreeLevelOrder(root1);
+	BinaryTreeLevelOrder(root2);
+	ret1 = BinaryTreeComplete(root1);
+	ret2 = BinaryTreeComplete(root2);
+	printf("ret1=%d ret2=%d\n", ret1, ret2);
+}
 int main()
 {
 	TestBinarryTree();
--- Btree/Btree/Stack.c
+++ Btree/Btree/Stack.c
@@ -1,79 +1,79 @@
-#include"Stack.h"
-#include<malloc.h>
-#include<assert.h>
-void StackInit(Stack* st)
-{
-	assert(st);
-	st->_a = NULL;
-	st->_capacity = st->_top = 0;
-}
-void StackDestory(Stack* st)
-{
-	assert(st);
-	free(st->_a);
-	st->_capacity = st->_top = 0;
-}
-void StackPush(Stack* st,DataType x)
-{
-	assert(st);
-	//ÈëÕ»£ºË³Ðò±íµÄÎ²²å
-	//¼ì²éÈÝÁ¿
-	if (st->_top == st->_capacity)
-	{
-		size_t newcapacity = st->_capacity == 0 ? 10 : 2 * st->_capacity;
-		st->_a = (DataType*)realloc(st->_a, newcapacity * sizeof(DataType));
-		st->_capacity = newcapacity;
-	}
-	//²åÈë
-	st->_a[st->_top++] = x;
-}
-void StackPop(Stack* st)
-{
-	assert(st);
-	//³öÕ»£ºË³Ðò±íÎ²É¾
-	if (st->_top > 0)
-	{
-		--st->_top;
-	}
-}
-DataType StackTop(Stack* st)
-{
-	assert(st);
-	return st->_a[st->_top - 1];
-}
-size_t StackSize(Stack* st)
-{
-	assert(st);
-	return st->_top;
-}
-int StackEmpty(Stack* st)
-{
-	assert(st);
-	return st->_top == 0 ? 1 : 0;
-}
-void StackPrint(Stack* st)
-{
-	assert(st);
-	while (StackEmpty(st) != 0)
-	{
-		printf("%d ", StackTop(st));
-		StackPop(st);
-	}
-	printf("\n");
-}
-//void TestStack()
+//#include"Stack.h"
+//#include<malloc.h>
+//#include<assert.h>
+//void StackInit(Stack* st)
 //{
-//	Stack s;
-//	StackInit(&s);
-//	StackPush(&s, 1);
-//	StackPush(&s, 2);
-//	StackPush(&s, 3);
-//	StackPush(&s, 4);
-//	StackPush(&s, 5);
-//	StackPrint(&s);
+//	assert(st);
+//	st->_a = NULL;
+//	st->_capacity = st->_top = 0;
 //}
-//int main()
+//void StackDestory(Stack* st)
 //{
-//	TestStack();
-//	return 0;
+//	assert(st);
+//	free(st->_a);
+//	st->_capacity = st->_top = 0;
 //}
+//void StackPush(Stack* st,DataType x)
+//{
+//	assert(st);
+//	//ÈëÕ»£ºË³Ðò±íµÄÎ²²å
+//	//¼ì²éÈÝÁ¿
+//	if (st->_top == st->_capacity)
+//	{
+//		size_t newcapacity = st->_capacity == 0 ? 10 : 2 * st->_capacity;
+//		st->_a = (DataType*)realloc(st->_a, newcapacity * sizeof(DataType));
+//		st->_capacity = newcapacity;
+//	}
+//	//²åÈë
+//	st->_a[st->_top++] = x;
+//}
+//void StackPop(Stack* st)
+//{
+//	assert(st);
+//	//³öÕ»£ºË³Ðò±íÎ²É¾
+//	if (st->_top > 0)
+//	{
+//		--st->_top;
+//	}
+//}
+//DataType StackTop(Stack* st)
+//{
+//	assert(st);
+//	return st->_a[st->_top - 1];
+//}
+//size_t StackSize(Stack* st)
+//{
+//	assert(st);
+//	return st->_top;
+//}
+//int StackEmpty(Stack* st)
+//{
+//	assert(st);
+//	return st->_top == 0 ? 1 : 0;
+//}
+//void StackPrint(Stack* st)
+//{
+//	assert(st);
+//	while (StackEmpty(st) != 0)
+//	{
+//		printf("%d ", StackTop(st));
+//		StackPop(st);
+//	}
+//	printf("\n");
+//}
+////void TestStack()
+////{
+////	Stack s;
+////	StackInit(&s);
+////	StackPush(&s, 1);
+////	StackPush(&s, 2);
+////	StackPush(&s, 3);
+////	StackPush(&s, 4);
+////	StackPush(&s, 5);
+////	StackPrint(&s);
+////}
+////int main()
+////{
+////	TestStack();
+////	return 0;
+////}
