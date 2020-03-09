e4e79efbbce4bfadf724e88aeb5eb329e9a89d15
Jamesuhao <1766727840@qq.com>
2020-03-09 22:27:39 +0800

    leetcode算法题练习

--- leetcode算法题/leetcode算法题/恢复二叉搜索树.cpp
+++ leetcode算法题/leetcode算法题/恢复二叉搜索树.cpp
@@ -28,3 +28,55 @@
     /
   3
 */
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+class Solution {
+public:
+    TreeNode* pre;
+    TreeNode* node1;
+    TreeNode* node2;
+    bool flag;
+    Solution()
+        :pre(nullptr)
+        , node1(nullptr)
+        , node2(nullptr)
+        , flag(false)
+    { }
+    void recoverTree(TreeNode* root)
+    {
+        inOrder(root);
+        int tmp = node1->val;
+        node1->val = node2->val;
+        node2->val = tmp;
+    }
+    void inOrder(TreeNode* root)
+    {
+        if (root == nullptr)
+        {
+            return;
+        }
+        inOrder(root->left);//L
+        if (pre != nullptr)
+        {
+            if (node1 == nullptr && pre->val > root->val)
+            {
+                node1 = pre;
+            }
+            if (!flag && node1 != nullptr && root->val > node1->val)
+            {
+                flag = true;
+                node2 = pre;
+            }
+            if (!flag)
+            {
+                node2 = root;
+            }
+        }
+        pre = root;
+        inOrder(root->right);
+    }
+};
